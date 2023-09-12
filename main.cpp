60ec6ac60c2579d239c44cc0a72be33864ddc4eb
Фёдор Абанин <fedor_a95@mail.ru>
2023-09-12 14:26:21 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+int main()
+{
+    int k;
+    cin>>k;
+    switch (k)
+    {
+        case 1:
+            int a = 0x123;
+
+
+    }
+}
