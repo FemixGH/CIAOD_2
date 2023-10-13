58cac93effd0fb6f072ba1d2b22f5d7bfc21a272
Фёдор Абанин <fedor_a95@mail.ru>
2023-10-13 15:14:13 +0300

    для егорчика

--- uga.cpp
+++ uga.cpp
@@ -1,57 +1,69 @@
-#include <random>
 #include <iostream>
-#include <chrono>
+#include <bitset>
+#include <vector>
+#include <iostream>
 #include <fstream>
+#include <string>
+#include <chrono>
+#include <random>
 
 using namespace std;
-int GetRandomNumber()
-{
+
+int getRandomNumber() {
     random_device rand_dev;
     mt19937 gen(rand_dev());
-    uniform_int_distribution random(10000, 999999);
+    uniform_int_distribution<int> random(100000, 999999);
     return random(gen);
 }
 
-void PerformTask() {
-    int numBytes = (pow(10, 7) + 7) / 8;
-    vector<int> numbers(numBytes);
-    ofstream file(R"(C:\Users\fedor\CLionProjects\CIAOD_2\numbers.txt)");
+void Perform() {
+    int bytes = (pow(10, 7) + 7) / 8;
+    vector<unsigned char> numbers(bytes);
+    ofstream outputFile(R"(C:\Users\fedor\CLionProjects\CIAOD_2\output.txt)");
+
     for (int i = 0; i < 1000000; i++) {
-        file << GetRandomNumber() << std::endl;
-    }
-    file.close();
-
-    ifstream inputFileStream("C:\\Users\\fedor\\CLionProjects\\CIAOD_2\\numbers.txt)\"");
-    fstream outputFile("(C:\\Users\\fedor\\CLionProjects\\CIAOD_2\\output.txt)");
-    string line;
-    int bitId, byteIndex, setBit;
-    while (getline(inputFileStream, line)) {
-        bitId = stoi(line);
-        byteIndex = bitId / 8;
-        setBit = bitId % 8;
-        numbers[byteIndex] |= (1 << setBit);
-    }
-    inputFileStream.close();
-
-    auto t0 = chrono::steady_clock::now();
-    for (int byteIndex = 0; byteIndex < numBytes; byteIndex++)
-    {
-        unsigned char byte = numbers[byteIndex];
-        for (int bitId = 0; bitId < 8; bitId++)
-        {
-            bool isBitSet = (byte & (1 << bitId));
-            if (isBitSet == 1)
-                outputFile << (byteIndex * 8 + bitId) << endl;
-        }
+        outputFile << getRandomNumber() << endl;
     }
     outputFile.close();
+
+    ifstream inputFile(R"(C:\Users\fedor\CLionProjects\CIAOD_2\numbers.txt)");
+    ofstream sortedOutputFile(R"(C:\Users\fedor\CLionProjects\CIAOD_2\sorted_output.txt)");
+    string line;
+
+    cout << "All files opened." << endl;
+
+    int bitIndex, byteIndex, setBit;
+    while (getline(inputFile, line)) {
+        bitIndex = stoi(line);
+        byteIndex = bitIndex / 8;
+        setBit = bitIndex % 8;
+        numbers[byteIndex] |= (1 << setBit);
+    }
+
+    inputFile.close();
+
+    cout << "Bits set." << endl;
+
+    auto t0 = chrono::steady_clock::now();
+    for (int byteIndex = 0; byteIndex < bytes; byteIndex++) {
+        unsigned char byte = numbers[byteIndex];
+        for (int bitIndex = 0; bitIndex < 8; bitIndex++) {
+            bool isBitSet = (byte & (1 << bitIndex));
+            if (isBitSet == 1)
+                sortedOutputFile << (byteIndex * 8 + bitIndex) << endl;
+        }
+    }
+
+    cout << "Sorted numbers written to file." << endl;
+
+    sortedOutputFile.close();
     auto t1 = chrono::steady_clock::now();
-    auto elapsed = chrono::duration_cast<std::chrono::microseconds>(t1 - t0);
-    cout << "lead time: " << elapsed.count() / 1000000.0 << " seconds" << endl;
-    cout << "Memory used: " << numbers.capacity() / (pow(2, 20)) << " MB" << endl;
+    auto elapsedTime = chrono::duration_cast<chrono::microseconds>(t1 - t0);
+    cout << "Execution time: " << elapsedTime.count() / 1000000.0 << " seconds." << endl;
+    cout << "Memory usage: " << numbers.capacity() / (pow(2, 20)) << " MB." << endl;
 }
 
 int main() {
-    PerformTask();
+    Perform();
     return 0;
 }
